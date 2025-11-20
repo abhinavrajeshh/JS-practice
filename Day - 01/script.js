25f12ee65b36e1cd6d223fc226b8d93052e295db
abhinavrajeshh <abhinav.x50@gmail.com>
2025-11-20 20:17:29 +0530

    Initial commit

---
+++ Day - 01/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello world!!!");
+//----------------------
+// alert("Hello sir")
+//-----------------------
+// "use strict";
+
+
+// let message = "Hello";
+// msg = "Hi";
+// console.log(msg)
+//-------------------
+
