a43564045c50d4325110fe87211b7246fc126eca
abhinavrajeshh <abhinav.x50@gmail.com>
2025-11-22 18:07:24 +0530

    updated session

---
+++ Session 2/script.js
@@ -0,0 +1,48 @@
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi); // What will be output?
+
+// -----------------------
+
+// console.log((4+6+ 9)/77);
+
+// let a = 10;
+// console.log(a);
+
+// a = "abhinav"
+
+// console.log(a);
+
+
+// const max = 57;
+// const actual = max - 13;
+// const percentage = actual / max;
+
+// console.log(percentage);
+
+
+// let day = 2 ;
+// switch (day){
+//     case 1:
+//         console.log("Its sunday");
+//         break;
+//         case 2:
+//             console.log("tuesday");
+//             break;
+//             default:
+//                 console.log("INVALID bro");
+// }
+
+// let age = 14;
+// let text1  = "You can drive"
+// let text2 = "You cannot drive"
+
+// if (age>=18){
+//     console.log(text1);
+// }else{
+//     console.log(text2);
+    
+// }
+
+
