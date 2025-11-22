2f603f5a4d5b00f51716716624e93e174747c4f3
abhinavrajeshh <abhinav.x50@gmail.com>
2025-11-22 20:24:13 +0530

    session 3 updated

---
+++ Session 3/new.js
@@ -0,0 +1,27 @@
+//-------- Loop scope---------
+// let i = 5;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Inside the loop: ", i);
+  
+// }
+
+
+// console.log("Outside the loop: ",i);
+// 1. for Loop
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }   
+
+// let i = 5;
+
+// for (let i = 0; i < 10; i++) {
+//      console.log("Inside the loop: ", i);
+// }
+
+//  console.log(i);
+// let i = 1;
+// while (i < 5) {
+//     console.log(i);
+//     i ++;
+// } 
